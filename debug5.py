f66a61bc730959803ac63de136991ad944e80fa8
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-10-11 12:39:53 -0700

    Update debug5.py

--- debug5.py
+++ debug5.py
@@ -6,5 +6,5 @@ total = 0
 for i in range(51):
     total = total + i
 else:
-          print("finished!")
-          print(f"The sum of all the numbers from 1 to 50 is {total}")
+    print("finished!")
+    print(f"The sum of all the numbers from 1 to 50 is {total}")
